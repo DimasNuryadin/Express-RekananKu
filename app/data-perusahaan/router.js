ddaada52c00137019e39f81a346e85eb88c00d24
DimasNuryadin <dimasnuryadinn@gmail.com>
2025-08-28 19:26:32 +0700

    implementasi authorization

--- app/data-perusahaan/router.js
+++ app/data-perusahaan/router.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { actionCreate, actionDelete, getDataPerusahaan, getAllDataPerusahaan, } = require('./controller');
+const { isLoginPlayer } = require('../middleware/auth');
 
 router.get('/', getAllDataPerusahaan);
-router.get('/:userId', getDataPerusahaan);
-router.post('/', actionCreate);
-router.delete('/:id', actionDelete);
+router.get('/:userId', isLoginPlayer, getDataPerusahaan);
+router.post('/', isLoginPlayer, actionCreate);
+router.delete('/:id', isLoginPlayer, actionDelete);
 
 module.exports = router;
